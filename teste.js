41b090be08b629e50966eafbe07d7a5d7947ef75
Dv <rb.davi24@gmail.com>
2026-04-22 17:38:50 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,4 +1,4 @@
-let exemplo1 = "1"
-let exemplo2 = 1
-console.log(exemplo1+exemplo2)
-console.log(exemplo2+exemplo2)
+console.log(exemplo2+exemplo2)let nota1 =  9
+let nota2 = 10
+let media = (nota1+nota2) / 2
+console.log("A media foi:" +media) 
