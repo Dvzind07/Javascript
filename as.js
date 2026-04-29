77ee491dbd31ba664643c50e5dfa684835372cd9
Dv <rb.davi24@gmail.com>
2026-04-29 17:04:51 -0300

    29/04

--- as.js
+++ as.js
@@ -1,4 +1,10 @@
-let exemplo1 = "1"
-let exemplo = 1
-console.log(exemplo+exemplo1)
-console.log(exemplo+exemplo)
+let economias = 0;
+let add1 = 50; 
+let poupanca = 2
+let gasteidinheiro = 30
+let total = economias + add1 * poupanca - gasteidinheiro
+console.log('Final do monte: R$',total);
+
+let temperatura1 = 67
+let temperatura2 = 30
+console.log("A temperatura esta maior que 30 ?",temperatura1>temperatura2)
