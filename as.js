e5687fa62d21905ae17812ac8275270e1aa49a61
Dv <rb.davi24@gmail.com>
2026-05-06 16:04:24 -0300

    Update as.js

--- as.js
+++ as.js
@@ -8,3 +8,7 @@ console.log('Final do monte: R$',total);
 let temperatura1 = 67
 let temperatura2 = 30
 console.log("A temperatura esta maior que 30 ?",temperatura1>temperatura2)
+
+let pontuacao = 120;
+
+console.log(pontuacao >= 100 ? "voce ganhou um premio" : "tente novamente");
