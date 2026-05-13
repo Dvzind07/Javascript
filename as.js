3990d93544a8e7d9ffd938d83718c64a2d2bd49f
Dv <rb.davi24@gmail.com>
2026-05-13 15:52:36 -0300

    Update as.js

--- as.js
+++ as.js
@@ -12,3 +12,11 @@ console.log("A temperatura esta maior que 30 ?",temperatura1>temperatura2)
 let pontuacao = 120;
 
 console.log(pontuacao >= 100 ? "voce ganhou um premio" : "tente novamente");
+
+let carro = {
+    modelo: "Pagani Zonda R",
+    ano: 2009,
+    automatico: false
+};
+
+console.log(`O carro é um ${carro.modelo}, do ano ${carro.ano}, e ele ${carro.automatico ? "é automático" : "não é automático"}.`);
