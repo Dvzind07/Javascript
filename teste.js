f119bf8837abac5de8a909c23c42882440dd9521
Dv <rb.davi24@gmail.com>
2026-05-20 16:45:23 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -11,3 +11,8 @@ console.log(`total a pagar: R$ ${total}`);
 let saldo = 100;
 saldo -= 20;
 console.log("Saldo atual:", saldo);
+
+for (let i = 0; i <= 10; i++) 
+    if (i % 2 === 0) {
+        console.log(i)
+    }
