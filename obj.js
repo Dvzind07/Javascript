ccb19a7c330e4bd1677738a4217baf8fac9fb95f
Dv <rb.davi24@gmail.com>
2026-05-24 19:04:30 -0300

    24/05

---
+++ obj.js
@@ -0,0 +1,31 @@
+let eu = {
+    nome: "Davi",
+    idade: 16,
+    craque: true
+};
+
+console.log(eu.nome);
+
+let pro = {
+    prod: "Livros",
+    prec: 30,
+    emest: true
+}
+console.log("Produto:",pro.prod);
+console.log("Preco R$:",pro.prec);
+
+let li = {
+    liv: "Contos de um mentiroso",
+    pag: 107,
+    aut: "Dv"
+}
+console.log(li.liv,"tem",li.pag,"paginas")
+li.genero = "Baseado em fatos reais ou nao"
+console.log(li)
+
+let ca = {
+    carr: "Pagani Zonda R",
+    a: 2009,
+    auto: false
+}
+console.log("O carro modelo",ca.carr,"do ano",ca.a,"e automatico ?",ca.auto)
