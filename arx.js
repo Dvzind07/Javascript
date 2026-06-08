c7672b99e684769d05efeab1646cd42fd739acd5
Dv <rb.davi24@gmail.com>
2026-06-07 21:24:23 -0300

    Mais estudo de coisas de javascript
    update 07/06

--- arx.js
+++ arx.js
@@ -44,6 +44,24 @@ console.log(cor.indexOf("branco"));
 let fraseparaela = ["Voce deixou um vazio","no meu peito que nao sai","nao consigo te esquecer","eu te amo mais que tudo nao queria que voce me abandonasse meu amor"]
 console.log(fraseparaela.join(" "));
 
+// Uso do splice retorna um parte do arrays sem modificicar o original, adiciona ou remove elementos do arrays
+let algo = ["pao","arroz","leite"];
+console.log(algo.splice(1,3));
+
+let algo2 = ["joao","maria","pedro"];
+algo2.splice(2,0,"ana");
+console.log(algo2);
+
+// Uso do ForEach executa uma funcao para cada item do arrays(nao retorna nada)
+let pess = ["bla1","bla2","bla3"];
+pess.forEach(nome => {
+    console.log("Mas fala um " + pess);
+});
+
+// Uso do map, cria um novo arrays com base na transformacao de cada item
+let numb = [1,3,8,9,10];
+let double = numb.map(n => n *2);
+console.log(double);
 
 
 
